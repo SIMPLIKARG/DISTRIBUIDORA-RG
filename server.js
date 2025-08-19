09a2a9350185e1c29582b3a48936137c76125261
SIMPLIKARG <gestion@simplikarg.com>
2025-08-19 00:31:55 -0300

    Borrado de archivos

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const { GoogleAuth } = require('google-auth-library');
-const { google } = require('googleapis');
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { GoogleAuth } from 'google-auth-library';
+import { google } from 'googleapis';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
