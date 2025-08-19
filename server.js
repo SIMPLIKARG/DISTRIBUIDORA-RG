2eb02c1655d39c0c1ed15013a111a11708535ccb
SIMPLIKARG <gestion@simplikarg.com>
2025-08-19 04:39:47 -0300

    Borrado de archivos

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Google Sheets setup
 let sheets = null;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./server.js",
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
